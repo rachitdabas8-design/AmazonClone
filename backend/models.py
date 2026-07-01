2616e712a3124d2d04ad71d86f79bc8607808c30
Rachit Dabas <rachitdabas8@gmail.com>
2026-07-01 17:01:01 +0530

    address-backend

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,5 @@
 from database import Base
-from sqlalchemy import Column, Integer, String 
-
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 class User(Base):
     __tablename__ = "users"
@@ -9,8 +8,19 @@ class User(Base):
     email = Column(String, unique=True, index=True)
 
 
-class Cart(Base):
+class Address(Base):
+    __tablename__ = "address"
 
+    id = Column(Integer, primary_key=True, index=True)
+    user_email = Column(String, ForeignKey("users.email"))
+
+    full_name = Column(String)
+    mobile = Column(String)
+    house = Column(String)
+    city = Column(String)
+    pincode = Column(String)
+
+class Cart(Base):
     __tablename__ = "cart"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
 
+
+# ---------------- USER SCHEMA ----------------
 class UserCreate(BaseModel):
     email: str
+class AddressCreate(BaseModel):
+    full_name: str
+    mobile: str
+    house: str
+    city: str
+    pincode: str
 
+# ---------------- CART SCHEMA ----------------
 class CartItem(BaseModel):
     name: str
     price: int
